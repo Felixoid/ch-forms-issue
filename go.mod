ada9a84c8be41054a008e1afca451702a88086c3
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-04-01 01:09:14 +0200

    Try to reproduce CH forms issue

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Felixoid/ch-forms-issue
+
+go 1.16
---
+++ reproduce.go
@@ -0,0 +1,85 @@
+// Package main provides issue reproducing
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"mime/multipart"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+func buildBody(lines int, u *url.URL) (*bytes.Buffer, string, error) {
+	body := new(bytes.Buffer)
+	header := ""
+	writer := multipart.NewWriter(body)
+	name := "metrics_list"
+	part, err := writer.CreateFormFile(name, name)
+	if err != nil {
+		return nil, header, err
+	}
+	data := []byte{}
+
+	for i := 1; i <= lines; i++ {
+		data = append(data, []byte("some pretty long line to fill the post\n")...)
+	}
+
+	// Send each table in separated form
+	_, err = part.Write(data)
+	if err != nil {
+		return nil, header, err
+	}
+
+	// Set name_format and name_structure for the table
+	q := u.Query()
+	q.Set(name+"_format", "TSV")
+	q.Set(name+"_structure", "Path String")
+	u.RawQuery = q.Encode()
+
+	err = writer.Close()
+	if err != nil {
+		return nil, header, err
+	}
+
+	header = writer.FormDataContentType()
+	return body, header, nil
+}
+
+func main() {
+	dsn := flag.String("url", "https://localhost:8443", "url to make request")
+	lines := flag.Int("lines", 40, "number of lines 'some pretty long line to fill the post' in request")
+	flag.Parse()
+	url, err := url.Parse(*dsn)
+	if err != nil {
+		return
+	}
+	q := url.Query()
+	q.Set("query", "SELECT Path, arrayFilter(x->isNotNull(x), anyOrNullResample(1615852800, 1617235199, 86400)(toUInt32(intDiv(Time, 86400)*86400), Time)), arrayFilter(x->isNotNull(x), avgOrNullResample(1615852800, 1617235199, 86400)(Value, Time)) FROM graphite.data PREWHERE Date >='2021-03-16' AND Date <= '2021-04-01' WHERE (Path in metrics_list) AND (Time >= 1615852800 AND Time <= 1617235199) GROUP BY Path FORMAT TSV")
+	url.RawQuery = q.Encode()
+	postBody, contentHeader, err := buildBody(*lines, url)
+	req, err := http.NewRequest("POST", url.String(), postBody)
+	req.Header.Add("Content-Type", contentHeader)
+	req.Header.Add("Content-Encoding", "gzip")
+	rawRequest, err := httputil.DumpRequestOut(req, true)
+	if err == nil {
+		fmt.Print(string(rawRequest))
+	}
+	client := &http.Client{
+		Timeout: time.Second * 50,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: time.Second * 3,
+			}).Dial,
+			DisableKeepAlives: true,
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	fmt.Print(resp)
+}
